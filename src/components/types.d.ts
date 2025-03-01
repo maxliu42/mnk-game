fbbf9f39f58202870dce658680a1cb8ee8314c9c
maxliu42 <maxliu42@gmail.com>
2025-02-28 23:03:51 -0500

    chore: update outdated type declarations

--- src/components/types.d.ts
+++ src/components/types.d.ts
@@ -1,18 +1,24 @@
 declare module './GameControls' {
+  import { GameConfig, PlayerConfig } from '../types/game.types';
+  
   interface GameControlsProps {
-    onStartGame: (m: number, n: number, k: number) => void;
+    onStartGame: (m: number, n: number, k: number, config?: GameConfig) => void;
+    defaultPlayerConfigs: PlayerConfig[];
   }
   const GameControls: React.FC<GameControlsProps>;
   export default GameControls;
 }
 
 declare module './GameInfo' {
+  import { MoveType, PlayerConfig } from '../types/game.types';
+  
   interface GameInfoProps {
     currentPlayer: number;
     winner: number | null;
     isDraw: boolean;
-    playerSymbols: string[];
+    playerConfigs: PlayerConfig[];
     onResetGame: (returnToMenu?: boolean) => void;
+    currentMoveType?: MoveType;
   }
   const GameInfo: React.FC<GameInfoProps>;
   export default GameInfo;
