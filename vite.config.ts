869157001574d1de119b79ddfd391971491aec03
maxliu42 <maxliu42@gmail.com>
2025-03-01 21:18:23 -0500

    fix: vite paths for github actions

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Base path for GitHub Pages deployment
+  base: '/mnk-game/', // Base path for GitHub Pages deployment
   server: {
     port: 3000,
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
   },
 }); 
