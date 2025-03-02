8c46b1107d7071c8d8ab4d5614977ff77674e649
maxliu42 <maxliu42@gmail.com>
2025-03-01 21:07:12 -0500

    fix: vite config base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mnk-game/', // Base path for GitHub Pages deployment
+  base: './', // Base path for GitHub Pages deployment
   server: {
     port: 3000,
   },
